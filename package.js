a62f97a1d84889a2e638619816f0fd526e443611
David Sykora <dsykora@gmail.com>
2015-11-13 03:32:05 -0800

    Get Presence working better for Meteor applications which are horizontally scaled with multiple servers.

--- package.js
+++ package.js
@@ -1,14 +1,17 @@
 Package.describe({
-  summary: "A package to help track users' presence"
+	summary: "A package to help track users' presence"
 });
 
-Package.on_use(function (api) {
-  api.add_files('lib/common.js', ['client', 'server']);
-  api.add_files('lib/client.js', 'client');
-  api.add_files('lib/server.js', 'server');
+Package.onUse(function (api) {
+	api.versionsFrom('1.0.2.1');
+	api.use('underscore', 'random');
+	api.addFiles('lib/common.js', ['client', 'server']);
+	api.addFiles('lib/client.js', 'client');
+	api.addFiles('lib/server.js', 'server');
 
-  if (typeof api.export !== 'undefined') {
-    api.export('Presences', ['client', 'server']);
-    api.export('Presence', ['client', 'server']);
-  }
+	if (typeof api.export !== 'undefined') {
+		api.export('Presences', ['client', 'server']);
+		api.export('ServerPresences', ['server']);
+		api.export('Presence', ['client', 'server']);
+	}
 });
